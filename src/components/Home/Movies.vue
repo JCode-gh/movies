f22e0da0fba399a31d4a161d8cd1bf9b4b053693
JarneStaalPXL <jarne.staal9@outlook.com>
2022-06-22 21:01:36 +0200

    More results

--- src/components/Home/Movies.vue
+++ src/components/Home/Movies.vue
@@ -3,26 +3,44 @@
     <div v-if="$store.state.hasResults === false">
       <h1 id="noresults">No results</h1>
     </div>
-    <div class="d-flex flex-wrap justify-content-center" v-if="$store.state.searchedResult.length > 0">
-      <div class="card" style="width: 18rem; margin: 20px"
-           v-for="(movie,index) in $store.state.searchedResult"
-           :style="{borderColor: isActive(movie) ? 'red' : 'darkgray'}" >
-        <router-link :to="'/movieinfo'+`?${movie.id}`">
-          <img class="image_placeholder card-img-top" :src="'https://image.tmdb.org/t/p/w1280/'+movie.poster_path" :alt="movie.title"/>
+    <div
+      class="d-flex flex-wrap justify-content-center"
+      v-if="$store.state.searchedResult.length > 0"
+    >
+      <div
+        class="card"
+        style="width: 18rem; margin: 20px"
+        v-for="(movie, index) in $store.state.searchedResult"
+        :style="{ borderColor: isActive(movie) ? 'red' : 'darkgray' }"
+      >
+        <router-link :to="'/movieinfo' + `?${movie.id}`">
+          <img
+            class="image_placeholder card-img-top"
+            :src="'https://image.tmdb.org/t/p/w1280/' + movie.poster_path"
+            :alt="movie.title"
+          />
         </router-link>
         <div class="card-body">
-          <router-link :to="'/movieinfo'+`?${movie.id}`">
+          <router-link :to="'/movieinfo' + `?${movie.id}`">
             <h5 class="card-title">{{ movie.title }}</h5>
           </router-link>
           <a @click="addToFavorites(movie, index)">
-            <font-awesome-icon class="faicons" :icon="['far', 'heart']"
-                               :style="{color: isActive(movie) ? 'red' : 'lightgray'}"  />
+            <font-awesome-icon
+              class="faicons"
+              :icon="['far', 'heart']"
+              :style="{ color: isActive(movie) ? 'red' : 'lightgray' }"
+            />
           </a>
         </div>
       </div>
     </div>
-    <div v-else-if="$store.state.hasResults === true && $store.state.searchedResult.length <= 0">
-      <LoadingSpinner/>
+    <div
+      v-else-if="
+        $store.state.hasResults === true &&
+        $store.state.searchedResult.length <= 0
+      "
+    >
+      <LoadingSpinner />
     </div>
   </div>
 </template>
@@ -34,23 +52,22 @@ export default {
   components: {
     LoadingSpinner,
   },
-  data(){
+  data() {
     return {
-      isAdded : Boolean,
-    }
+      isAdded: Boolean,
+    };
   },
-  methods : {
+  methods: {
     isActive(movie) {
-      if(!movie) return false
-      const storeData = this.$store.state.favoriteMovies || []
-      return storeData.some(obj => obj.id === movie.id)
+      if (!movie) return false;
+      const storeData = this.$store.state.favoriteMovies || [];
+      return storeData.some((obj) => obj.id === movie.id);
     },
     async addToFavorites(movie) {
-      if (this.$store.state.favoriteMovies.find(mov => mov.id === movie.id)){
+      if (this.$store.state.favoriteMovies.find((mov) => mov.id === movie.id)) {
         await this.$store.commit("REMOVE_FAVMOVIE", movie);
-      }
-      else {
-        await this.$store.commit('SET_FAVMOVIES', movie);
+      } else {
+        await this.$store.commit("SET_FAVMOVIES", movie);
       }
     },
   },
@@ -59,29 +76,33 @@ export default {
     if (localStorage.getItem("genreIdSelected")) {
       let genreId = localStorage.getItem("genreIdSelected");
 
-      await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${this.$store.state.apiKey}&with_genres=${genreId}`)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(movies => {
-            this.$store.commit("INSERT_MOVIES_SEARCHEDRESULT", movies.results);
-          })
+      await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${this.$store.state.apiKey}&with_genres=${genreId}`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then((movies) => {
+          console.log(movies);
+          this.$store.commit("INSERT_MOVIES_SEARCHEDRESULT", movies.results);
+        });
     } else {
-      await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${this.$store.state.apiKey}&language=en-US&page=1`)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(movies => {
-            this.$store.commit("INSERT_MOVIES_SEARCHEDRESULT", movies.results)
-          })
+      await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${this.$store.state.apiKey}&language=en-US&page=1`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then((movies) => {
+          this.$store.commit("INSERT_MOVIES_SEARCHEDRESULT", movies.results);
+        });
     }
 
     if (localStorage.getItem("favoriteMovies")) {
       this.$store.commit("GET_FAVMOVIES_FROM_LST");
     }
-
   },
-}
+};
 </script>
 
 <style scoped>
@@ -93,8 +114,8 @@ export default {
   width: 100%;
   transform: translate(-50%, -50%);
 }
-.image_placeholder{
-  background: url('../../assets/placeholder.png') no-repeat !important;
+.image_placeholder {
+  background: url("../../assets/placeholder.png") no-repeat !important;
 }
 
 .fullMovieContainer {
@@ -110,14 +131,13 @@ export default {
 a {
   text-decoration: none;
   color: white;
-
 }
 .card {
   background-color: black;
   border: 3px solid white;
 }
 img {
-  transition: transform .2s;
+  transition: transform 0.2s;
 }
 img:hover {
   transform: scale(0.98);
--- src/components/Home/Nav.vue
+++ src/components/Home/Nav.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="nav">
     <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item navbar-brand">
-            <input type="button" class="nav-link" value="Movies"
-                   style="background: none;color: red;outline: none;border: none" @click="showPopularMovies">
+            <input
+              type="button"
+              class="nav-link"
+              value="Movies"
+              style="background: none; color: red; outline: none; border: none"
+              @click="showPopularMovies"
+            />
           </li>
-          <li class="nav-item navbar-brand d-flex flex-column-reverse"  v-if="$store.state.favoriteMovies.length > 0">
-            <input type="button" class="nav-link" :value="'Favorites'+ '('+ $store.state.favoriteMovies.length  +')'"
-                   style="background: none;color: red;outline: none;border: none" @click="pushFavorites">
+          <li
+            class="nav-item navbar-brand d-flex flex-column-reverse"
+            v-if="$store.state.favoriteMovies.length > 0"
+          >
+            <input
+              type="button"
+              class="nav-link"
+              :value="
+                'Favorites' + '(' + $store.state.favoriteMovies.length + ')'
+              "
+              style="background: none; color: red; outline: none; border: none"
+              @click="pushFavorites"
+            />
           </li>
         </ul>
-          <input class="form-control mr-sm-2" v-model="userInput" placeholder="Search" aria-label="Search" @keypress="kpHandler">
-          <input type="button" style="color: white" class="btn bg-gradient bg-danger btn-outline-danger my-2 my-sm-0"  @click="searchClicked" value="Search">
+        <input
+          class="form-control mr-sm-2"
+          v-model="userInput"
+          placeholder="Search"
+          aria-label="Search"
+          @keypress="kpHandler"
+        />
+        <input
+          type="button"
+          style="color: white"
+          class="btn bg-gradient bg-danger btn-outline-danger my-2 my-sm-0"
+          @click="searchClicked"
+          value="Search"
+        />
       </div>
     </nav>
-    <nav class="d-flex justify-content-center p-2 fixed-bottom" id="filter" v-if="genresList.genres">
+    <nav
+      class="d-flex justify-content-center p-2 fixed-bottom"
+      id="filter"
+      v-if="genresList.genres"
+    >
       <h5 id="genre">Genre</h5>
       <select id="genreSelect" @change="genreChange" class="form-select">
-        <option v-for="genre in genresList.genres" :value="genre.id" :key="genre.id">{{genre.name}}</option>
+        <option
+          v-for="genre in genresList.genres"
+          :value="genre.id"
+          :key="genre.id"
+        >
+          {{ genre.name }}
+        </option>
       </select>
     </nav>
   </div>
@@ -33,52 +78,78 @@ import router from "../../router";
 
 export default {
   name: "Nav",
-  data(){
+  data() {
     return {
-      userInput : "",
-      genresList : []
-    }
+      userInput: "",
+      genresList: [],
+    };
   },
-  methods : {
+  methods: {
     async genreChange(event) {
-      localStorage.setItem('genreIdSelected', document.querySelector("select").value);
-      localStorage.setItem('genreIndexSelected', document.getElementById("genreSelect").selectedIndex)
+      localStorage.setItem(
+        "genreIdSelected",
+        document.querySelector("select").value
+      );
+      localStorage.setItem(
+        "genreIndexSelected",
+        document.getElementById("genreSelect").selectedIndex
+      );
 
       this.$store.commit("CLEAR_SEARCHEDRESULT");
       const genreId = event.target.value;
-      await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${this.$store.state.apiKey}&with_genres=${genreId}`)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(movies => {
-            this.$store.commit("INSERT_MOVIES_SEARCHEDRESULT", movies.results);
-          })
+      await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${this.$store.state.apiKey}&with_genres=${genreId}`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(async (movies) => {
+          await fetch(
+            `https://api.themoviedb.org/3/discover/movie?api_key=${this.$store.state.apiKey}&with_genres=${genreId}&page=2`
+          )
+            .then(function (response) {
+              return response.json();
+            })
+            .then((secondMovies) => {
+              let arr = [];
 
+              console.log(secondMovies);
+              for (let mov of movies.results) {
+                arr.push(mov);
+              }
+              for (let movie of secondMovies.results) {
+                arr.push(movie);
+              }
+              console.log(arr.length);
+              this.$store.commit("INSERT_MOVIES_SEARCHEDRESULT", arr);
+            });
+        });
     },
-    pushFavorites(){
-      this.$router.push('/favorites');
+    pushFavorites() {
+      this.$router.push("/favorites");
     },
     async showPopularMovies() {
-      this.$router.push('/');
+      this.$router.push("/");
       localStorage.removeItem("genreIndexSelected");
       localStorage.removeItem("genreIdSelected");
       this.$store.commit("CLEAR_USERINPUT");
-      document.querySelector('.form-control').value = "";
+      document.querySelector(".form-control").value = "";
       this.$store.commit("SET_HASRESULTS_TRUE");
       this.$store.commit("CLEAR_SEARCHEDRESULT");
-      await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${this.$store.state.apiKey}&language=en-US&page=1`)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(movies => {
-            this.$store.commit("INSERT_MOVIES_SEARCHEDRESULT", movies.results)
-          })
+      await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${this.$store.state.apiKey}&language=en-US&page=1`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then((movies) => {
+          this.$store.commit("INSERT_MOVIES_SEARCHEDRESULT", movies.results);
+        });
 
       document.getElementById("genreSelect").selectedIndex = 0;
     },
-    kpHandler(e){
-      if (e.key === 'Enter')
-        this.searchClicked();
+    kpHandler(e) {
+      if (e.key === "Enter") this.searchClicked();
     },
     async searchClicked() {
       if (this.userInput !== this.$store.state.userinput) {
@@ -86,33 +157,42 @@ export default {
         localStorage.removeItem("genreIndexSelected");
         this.$store.state.userinput = this.userInput;
         this.$store.commit("CLEAR_SEARCHEDRESULT");
-        await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.$store.state.apiKey}&language=en-US&query=${this.$store.state.userinput}&page=1&include_adult=false`)
-            .then(function (response) {
-              return response.json();
-            })
-            .then(movies => {
-              this.$store.commit("MAKE_UNIQUE_SEARCHEDRESULT", movies.results);
-            })
+        await fetch(
+          `https://api.themoviedb.org/3/search/movie?api_key=${this.$store.state.apiKey}&language=en-US&query=${this.$store.state.userinput}&page=1&include_adult=false`
+        )
+          .then(function (response) {
+            return response.json();
+          })
+          .then((movies) => {
+            this.$store.commit("MAKE_UNIQUE_SEARCHEDRESULT", movies.results);
+          });
       }
-    }
+    },
   },
   async beforeMount() {
-    await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${this.$store.state.apiKey}&language=en-US`)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(genreListFull => {
-          this.genresList = genreListFull;
-        })
+    await fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${this.$store.state.apiKey}&language=en-US`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then((genreListFull) => {
+        this.genresList = genreListFull;
+      });
   },
   updated() {
     this.$nextTick(function () {
-      if (document.getElementById("genreSelect") !== null && localStorage.getItem("genreIndexSelected") !== null){
-        document.getElementById("genreSelect").selectedIndex = parseInt(localStorage.getItem("genreIndexSelected"));
+      if (
+        document.getElementById("genreSelect") !== null &&
+        localStorage.getItem("genreIndexSelected") !== null
+      ) {
+        document.getElementById("genreSelect").selectedIndex = parseInt(
+          localStorage.getItem("genreIndexSelected")
+        );
       }
-    })
+    });
   },
-}
+};
 </script>
 
 <style scoped>
@@ -121,7 +201,7 @@ export default {
   padding: 17px 9px 5px 5px;
   margin: auto;
   color: black;
-  border-radius: .25rem;
+  border-radius: 0.25rem;
 }
 select {
   outline: none;
@@ -141,53 +221,55 @@ option:hover {
   color: lightgrey;
 }
 sup {
-padding: 10px;
+  padding: 10px;
 }
 #navbarSupportedContent {
-justify-content: flex-end;
-margin-right: 3em;
+  justify-content: flex-end;
+  margin-right: 3em;
 }
 
 .navbar-brand {
-margin-left: 3rem;
+  margin-left: 3rem;
 }
 
-.form-control, .form{
-margin-right: 1rem;
+.form-control,
+.form {
+  margin-right: 1rem;
 }
 #nav a {
-font-weight: bold;
-color: #2c3e50;
+  font-weight: bold;
+  color: #2c3e50;
 }
 nav {
-background: black !important;
+  background: black !important;
 }
 a {
-text-decoration: none;
+  text-decoration: none;
 }
 li {
-padding-right: 40px;
+  padding-right: 40px;
 }
 #nav a.router-link-exact-active {
-color: red;
+  color: red;
 }
 
-@media only screen and (max-width: 992px){
-.navbar-toggler {
-display: none;
-}
+@media only screen and (max-width: 992px) {
+  .navbar-toggler {
+    display: none;
+  }
 
-#navbarSupportedContent{
-margin-right: 0;
-}
-.navbar-nav {
-display: flex;
-flex-direction: row;
-justify-content: center;
-}
-.form-control, .bg-danger {
-width: 75%;
-margin: auto;
-}
+  #navbarSupportedContent {
+    margin-right: 0;
+  }
+  .navbar-nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .form-control,
+  .bg-danger {
+    width: 75%;
+    margin: auto;
+  }
 }
 </style>
